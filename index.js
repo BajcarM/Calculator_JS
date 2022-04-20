bfd003615c0eda423ac85e7f39f0397e53ec3f60
BajcarM <102542768+BajcarM@users.noreply.github.com>
2022-04-20 08:52:55 +0200

    Shorten the code

--- index.js
+++ index.js
@@ -1,119 +1,84 @@
-// const numberBtn = document.getElementById("number-btn");
-// const operationBtn = document.getElementById("operation-btn");
-const inputNumber = document.getElementById("input-number");
-const results = document.getElementById("results");
+class Calculator {
+  static inputNumber = document.getElementById("input-number");
+  static results = document.getElementById("results");
 
-let inputString = "0";
-let compArray = [];
-let decimalPoint = false;
-let emptyInput = true;
-let emptyResult = true;
-renderResults();
+  static inputString = "0";
+  static compArray = [];
 
-function renderResults() {
-  let resultsText = "";
-  for (i = 0; i < compArray.length; i++) {
-    resultsText += compArray[i] + " ";
-  }
-  results.innerHTML = resultsText;
-  inputNumber.innerHTML = inputString;
-}
-
-function numBtn(number) {
-  if (emptyInput === true && number === 0) {
-    inputString = "0";
-  } else if (emptyInput === false) {
-    inputString += number;
-  } else if (
-    (emptyResult === true || compArray.length == 2) &&
-    emptyInput === true
-  ) {
-    inputString = "";
-    inputString += number;
-    emptyInput = false;
-  } else if (compArray.length == 1 && emptyInput === true) {
-    clearEverything();
-    inputString = "";
-    inputString += number;
-    emptyInput = false;
-  }
-  //   } else if (emptyResult === true && emptyInput === false) {
-  //     inputString += number;
-  //   } else if (compArray.length == 2 && emptyInput === true) {
-  //     inputString = "";
-  //     inputString += number;
-  //     emptyInput = false;
-
-  renderResults();
-}
-
-function opeBtn(sign) {
-  if (sign === "decimal-point" && decimalPoint === false) {
-    inputString += ".";
-    decimalPoint = true;
-    emptyInput = false;
-  } else if (
-    sign === "equal" &&
-    emptyInput === false &&
-    emptyResult === false
-  ) {
-    equals(compArray[0], Number(inputString), compArray[1]);
-  } else if (sign === "C") {
-    clear();
-  } else if (sign === "CE") {
-    clearEverything();
-  } else if (sign === "+" || sign === "-" || sign === "×" || sign === "÷") {
-    operator(sign);
+  static renderResults() {
+    let resultsText = "";
+    for (let i = 0; i < this.compArray.length; i++) {
+      resultsText += this.compArray[i] + " ";
+    }
+    results.innerHTML = resultsText;
+    this.inputNumber.innerHTML = this.inputString;
   }
 
-  renderResults();
-}
-
-function equals(num1, num2, sign) {
-  let compResult = 0;
-  if (sign === "+") {
-    compResult = num1 + num2;
-  } else if (sign === "-") {
-    compResult = num1 - num2;
-  } else if (sign === "×") {
-    compResult = num1 * num2;
-  } else if (sign === "÷") {
-    compResult = num1 / num2;
+  static numBtn(number) {
+    if (this.inputString !== "0") {
+      this.inputString += number;
+    } else if (this.compArray.length < 1 || this.compArray.length === 2) {
+      this.inputString = "" + number;
+    } else if (this.compArray.length === 1) {
+      this.clearEverything();
+      this.inputString = "" + number;
+    }
+    this.renderResults();
   }
-  compArray = [];
-  inputString = "0";
-  compArray[0] = compResult;
-  decimalPoint = false;
-  emptyInput = true;
-  emptyResult = false;
-}
 
-function clear() {
-  inputString = "0";
-  decimalPoint = false;
-  emptyInput = true;
-}
+  static handlerDecimalPoint() {
+    if (this.inputString.indexOf(".") >= 0) {
+      return;
+    }
+    this.inputString += ".";
+    this.renderResults();
+  }
 
-function clearEverything() {
-  inputString = "0";
-  decimalPoint = false;
-  emptyInput = true;
-  compArray = [];
-  emptyResult = true;
-}
+  static calculate(num1, num2, sign) {
+    if (sign === "+") {
+      return num1 + num2;
+    } else if (sign === "-") {
+      return num1 - num2;
+    } else if (sign === "×") {
+      return num1 * num2;
+    } else if (sign === "÷") {
+      return num1 / num2;
+    }
+  }
 
-function operator(sign) {
-  if (emptyResult) {
-    compArray[0] = Number(inputString);
-    compArray[1] = sign;
-    emptyResult = false;
-    inputString = "0";
-    emptyInput = true;
-    decimalPoint = false;
-  } else if (emptyResult === false && emptyInput === true) {
-    compArray[1] = sign;
-  } else if (emptyResult === false && emptyInput === false) {
-    equals(compArray[0], Number(inputString), compArray[1]);
-    compArray[1] = sign;
+  static equals() {
+    if (this.inputString === "0" || this.compArray.length < 1) {
+      return;
+    }
+    let sup = this.calculate(
+      this.compArray[0],
+      Number(this.inputString),
+      this.compArray[1]
+    );
+    this.clearEverything();
+    this.compArray[0] = sup;
+    this.renderResults();
+  }
+
+  static clearInput() {
+    this.inputString = "0";
+    this.renderResults();
+  }
+
+  static clearEverything() {
+    this.compArray = [];
+    this.clearInput();
+  }
+
+  static operator(sign) {
+    if (this.compArray.length < 1) {
+      this.compArray[0] = Number(this.inputString);
+      this.clearInput();
+    } else if (this.inputString !== "0") {
+      this.equals();
+    }
+    this.compArray[1] = sign;
+    this.renderResults();
   }
 }
+Calculator.renderResults();
